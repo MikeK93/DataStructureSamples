abaf58b2dc9073fbf7bd43d5ab22912f6fc7534a
Mikhail Kozlov <mestremi6a@gmail.com>
2017-05-27 19:51:28 +0300

    Handle default values for Key and Value

--- DataStructures/Source/HashTable/Entry.cs
+++ DataStructures/Source/HashTable/Entry.cs
@@ -25,7 +25,10 @@ namespace DataStructures.Source.HashTable
 
         public bool Equals(Entry<TKey, TValue> obj)
         {
-            return obj != null && Key.Equals(obj.Key) && Value.Equals(obj.Value);
+            return obj != null && 
+                ReferenceEquals(this, obj) && 
+                (!EqualityComparer<TKey>.Default.Equals(Key, default(TKey)) && Key.Equals(obj.Key)) &&
+                (!EqualityComparer<TValue>.Default.Equals(Value, default(TValue)) && Value.Equals(obj.Value));
         }
 
         public override int GetHashCode()
